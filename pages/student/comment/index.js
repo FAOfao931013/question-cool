a60d0897d1c6ee0d32a77b652ab240098d162d15
fuhao <465476662@qq.com>
2017-09-02 15:14:47 +0800

    完成学生查看答题反馈情况

---
+++ pages/student/comment/index.js
@@ -0,0 +1,94 @@
+/*eslint-disable */
+import Answer from '../../../model/answer.js';
+
+const AV = require('../../../lib/av-weapp-min.js');
+
+const app = getApp();
+
+Page({
+	data: {
+		imgSrc: '../../../img/',
+		id: '',
+		question: {},
+		answer: {},
+		comment: {}
+	},
+	//预览答题照片
+	previewImage(e) {
+		return app.previewImage({
+			current: e.currentTarget.id,
+			urls: [e.currentTarget.id]
+		})
+	},
+	//获取当前回答
+	getAnswer(id) {
+		var answerQuery = new AV.Query('Answer');
+
+		answerQuery.equalTo('objectId', id);
+
+		answerQuery.find().then(res => {
+			this.setData({
+				answer: res[0].attributes,
+			});
+		});
+	},
+	//获取当前问题
+	getQuestion(id) {
+		const question = new AV.Query('Question');
+
+		question.equalTo('objectId', id);
+
+		question.find().then(res => {
+			this.setData({
+				question: res[0].attributes
+			});
+		});
+	},
+	//获取反馈
+	getComment() {
+		const {
+			id,
+		} = this.data;
+
+		const queryOne = new AV.Query('Comment');
+		const queryTwo = new AV.Query('Comment');
+
+		queryOne.equalTo('username', app.globalData.user.username);
+
+		queryTwo.equalTo('questionId', id);
+
+		const commentQuery = AV.Query.and(queryOne, queryTwo);
+
+		commentQuery.find().then(res => {
+			this.setData({
+				comment: res[0].attributes,
+			});
+
+			this.getQuestion(res[0].attributes.questionId);
+			this.getAnswer(res[0].attributes.answerId);
+		});
+	},
+	onLoad(options) {
+		this.setData({
+			id: options.id
+		});
+	},
+	onReady() {
+
+	},
+	onShow() {
+		this.getComment();
+	},
+	onHide() {
+
+	},
+	onUnload() {
+
+	},
+	onPullDownRefresh() {
+
+	},
+	onReachBottom() {
+
+	},
+})
